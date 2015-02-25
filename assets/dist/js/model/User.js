578c1d451992654824428cc648c4c89231d9712a
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-02-25 18:38:19 -0300

    Remove dist/

--- assets/dist/js/model/User.js
+++
@@ -1,1 +0,0 @@
-define("model/user",["backbone"],function(Backbone){"use strict";var UserModel=Backbone.Model.extend({defaults:{name:"",email:""},url:function(){return window.location.origin+window.location.pathname+"api/users"}});return UserModel});
